f93de5b58ce7dac8dcc64a080674b6d3bf70f399
moeidsaleem <mashreq.shabeer@gmail.com>
2019-03-03 10:54:05 +0400

    updates

--- sample_application/config.js
+++ sample_application/config.js
@@ -3,11 +3,11 @@ var paysafeApiKeySecret;
 var paysafeAccountNumber;
 var paysafeEnvironment;
 
-exports.paysafeApiKeyId = 'your-api-key';
-exports.paysafeApiKeySecret = 'your-api-key-secret';
-exports.paysafeAccountNumber = 'your-account-number';
+exports.paysafeApiKeyId = 'test_moeidsaleem';
+exports.paysafeApiKeySecret = 'B-qa2-0-5c7140ad-1-302c02143a0af099dac88bbc5c24bf7d0b1407f0d45d502d02144a23932045cf6b1b517774a003b02f41c7c62678';
+exports.paysafeAccountNumber = '1001339120';
 exports.paysafeEnvironment = 'TEST';// TEST or LIVE
 
 
-exports.currency_code = 'currency-code';
+exports.currency_code = 'USD';
 exports.currency_base_units_multiplier = 100;
